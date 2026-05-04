3eb423e0f825fa467a15fa6a5fef74fb0c973283
Daniele <danielemarinelli1387@gmail.com>
2026-05-04 15:10:49 +0200

    added config.py file

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config:
+    valid_email="pavanol@abc.com"
+    valid_password="test@123"
+    invalid_email="tester123@qa.com"
+    invalid_password="test@123xyz"
+    product_name = "MacBook"
+    product_quantity = "1"
+    total_price = "$602.00"
